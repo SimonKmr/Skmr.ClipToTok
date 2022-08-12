8230c341d16a0cfa867fd670063267758f3024b5
SimonKmr <darkfish070@gmail.com>
2022-08-12 14:25:23 +0200

    Added Video Playback

--- Skmr.ClipToTok.WPF/MainWindow.xaml.cs
+++ Skmr.ClipToTok.WPF/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
-﻿using ReactiveUI;
+﻿using LibVLCSharp.Shared;
+using ReactiveUI;
 using SkiaSharp;
 using SkiaSharp.Views.Desktop;
 using SkiaSharp.Views.WPF;
 using Skmr.ClipToTok.ViewModels;
+using System;
 using System.Diagnostics;
+using System.IO;
 using System.Windows;
 using System.Windows.Navigation;
 
@@ -17,19 +20,40 @@ namespace Skmr.ClipToTok.WPF
         public MainWindow()
         {
             InitializeComponent();
+            //ViewModel
             var vm = new MainViewModel();
             DataContext = vm;
             vm.Settings.ScreenPosWebcam.OnScreenPosChanged += ScreenPosWebcam_OnScreenPosChanged;
             vm.Settings.ScreenPosGameplay.OnScreenPosChanged += ScreenPosGameplay_OnScreenPosChanged;
+            //Skia
             canvasView.SizeChanged += CanvasView_SizeChanged;
+            //Vlc Player
+            videoView.Loaded += VideoView_Loaded;
         }
 
 
+        LibVLC _libVLC;
+        MediaPlayer _mediaPlayer;
 
-        private void CanvasView_SizeChanged(object sender, System.Windows.SizeChangedEventArgs e)
+        void VideoView_Loaded(object sender, RoutedEventArgs e)
+        {
+            Core.Initialize();
+
+            _libVLC = new LibVLC();
+            _mediaPlayer = new MediaPlayer(_libVLC);
+
+            videoView.MediaPlayer = _mediaPlayer;
+        }
+
+
+        private void CanvasView_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             var element = sender as SKElement;
-            element.Height = e.NewSize.Width * 9f / 16f;
+            //element.Height = e.NewSize.Width * 9f / 16f;
+
+            //if hight has margin, make width the resize factor
+            //if width has margin, make height the resize factor
+
         }
 
         private void ScreenPosWebcam_OnScreenPosChanged(int x, int y, int width, int height)
@@ -77,6 +101,7 @@ namespace Skmr.ClipToTok.WPF
             canvas.DrawRect(xGamep * x1, yGamep * y1, widthGamep * x1, heightGamep * y1, new SKPaint() { Color = new SKColor(0, 255, 0, 122) });
         }
 
+        #region Drag and Drops
         private void SourceVideo_Drop(object sender, System.Windows.DragEventArgs e)
         {
             txtSourceVideo.Text = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
@@ -96,6 +121,7 @@ namespace Skmr.ClipToTok.WPF
         {
             txtBackgroundImage.Text = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
         }
+        #endregion
 
         private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
         {
@@ -106,6 +132,51 @@ namespace Skmr.ClipToTok.WPF
             psi.FileName = e.Uri.AbsoluteUri;
             Process.Start(psi);
             e.Handled = true;
+        }
+
+        private bool isPlaying = false;
+        private string playedBackVideo = String.Empty;
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(txtSourceVideo.Text))
+            {
+                if (!playedBackVideo.Equals(txtSourceVideo.Text))
+                {
+                    _mediaPlayer.Play(new Media(_libVLC, new Uri(txtSourceVideo.Text)));
+                    playedBackVideo = txtSourceVideo.Text;
+                    isPlaying = true;
+                }
+                else if(!isPlaying && playedBackVideo.Equals(txtSourceVideo.Text))
+                {
+                    _mediaPlayer.Play();
+                    isPlaying = true;
+                }
+                else
+                {
+                    _mediaPlayer.Pause();
+                }
+            } 
+            else _mediaPlayer.Stop();
+        }
+
+        private void Border_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var normalizedWidth = e.NewSize.Width / 16d;
+            var normalizedHeight = e.NewSize.Height / 9d;
+            if (normalizedHeight > normalizedWidth)
+            {
+                var height = videoView.ActualWidth * 9d / 16d;
+                var width = videoView.ActualWidth;
+                canvasView.Height = height;
+                canvasView.Width = width;
+            }
+            else
+            {
+                var height = videoView.ActualHeight;
+                var width = videoView.ActualHeight * 16d / 9d;
+                canvasView.Height = height;
+                canvasView.Width = width;
+            }
         }
     }
 }
