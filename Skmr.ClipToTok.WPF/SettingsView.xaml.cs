58c64bc95a5a8372b5f5edb0f01f45f43bad7388
SimonKmr <darkfish070@gmail.com>
2022-08-13 13:39:21 +0200

    added bindings

--- Skmr.ClipToTok.WPF/SettingsView.xaml.cs
+++ Skmr.ClipToTok.WPF/SettingsView.xaml.cs
@@ -4,6 +4,7 @@ using Splat;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Disposables;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,6 +27,39 @@ namespace Skmr.ClipToTok.WPF
         public SettingsView()
         {
             InitializeComponent();
+
+            this.WhenActivated(d =>
+            {
+                this.Bind(ViewModel, vm => vm.VideoFile, v => v.txtSourceVideo.Text).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.Resolution, v => v.txtResolution.Text).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.ResultFolder, v => v.txtResFolder.Text).DisposeWith(d);
+
+                this.Bind(ViewModel, vm => vm.HasTimeFrame, v => v.cbTimeFrameEnabled.IsChecked).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.TimeFrameStart, v => v.txtTimeFrameStart.Text).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.TimeFrameDuration, v => v.txtTimeFrameDuration.Text).DisposeWith(d);
+                
+                this.Bind(ViewModel, vm => vm.HasChangeSpeed, v => v.cbSpeed.IsChecked).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.Speed, v => v.slSpeed.Value).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.Speed, v => v.txtSpeed.Text).DisposeWith(d);
+                
+                this.Bind(ViewModel, vm => vm.HasColorGrading, v => v.cbColorGrading.IsChecked).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.Contrast, v => v.txtContrast.Text).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.Contrast, v => v.slContrast.Value).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.Brighness, v => v.txtBrightness.Text).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.Brighness, v => v.slBrightness.Value).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.Saturation, v => v.txtSaturation.Text).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.Saturation, v => v.slSaturation.Value).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.Gamma, v => v.txtGamma.Text).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.Gamma, v => v.slGamma.Value).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.GammaR, v => v.txtGammaR.Text).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.GammaR, v => v.slGammaR.Value).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.GammaG, v => v.txtGammaG.Text).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.GammaG, v => v.slGammaG.Value).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.GammaB, v => v.txtGammaB.Text).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.GammaB, v => v.slGammaB.Value).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.GammaWeight, v => v.txtGammaWeight.Text).DisposeWith(d);
+                this.Bind(ViewModel, vm => vm.GammaWeight, v => v.slGammaWeight.Value).DisposeWith(d);
+            });
         }
 
         #region Drag and Drops
