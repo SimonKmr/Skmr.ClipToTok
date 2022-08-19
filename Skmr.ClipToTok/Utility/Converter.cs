a89f71677e2634f9041bc6592882b238be2731e7
SimonKmr <darkfish070@gmail.com>
2022-08-19 08:42:27 +0200

    Added

---
+++ Skmr.ClipToTok/Utility/Converter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skmr.ClipToTok.Utility
+{
+    public static class Converter
+    {
+        public static Settings ToSettings(this ViewModels.SettingsViewModel settingsViewModel)
+        {
+            throw new NotImplementedException();
+        }
+        public static ViewModels.SettingsViewModel ToSettingsViewModel(this Settings settings)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Highlight ToHighlight(this ViewModels.HighlightViewModel highlightViewModel)
+        {
+            throw new NotImplementedException();
+        }
+        public static ViewModels.HighlightViewModel ToHighlightViewModel(this Highlight highlightViewModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
